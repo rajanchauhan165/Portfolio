52c3fa62bc995184337bae81f25f55811eeaccbb
rajan chauhan <rajanchauhan165@gmail.com>
2023-01-07 21:26:22 +0530

    new portfolio added

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 700, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+
+function git(){
+    window.open("https://github.com/rajanchauhan165","_blank")
+}
+function res(){
+    window.open("https://drive.google.com/file/d/18rWcBNNIykBi2k5jKwmOAfipzPWHl2Eq/view?usp=sharing","_blank")
+}
+function hire(){
+    window.open("mailto:rajanchauhan165@gmail.com?subject=I found your profile online and I'd like to...&body=Hi Rajan,")
+}
+
+function sendEmail(){
+    window.open("mailto:rajanchauhan165@gmail.com?subject=PORTFOLIO&body=Name: "+document.getElementById("cformname").value
+    +"<br> Email: "+document.getElementById("cformemail").value +"<br> Message: "+document.getElementById("cformmessage").value)
+}
